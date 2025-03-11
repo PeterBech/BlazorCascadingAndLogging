715c7f002eb70a169f30c343dc133687a632b043
PeterBech <pibm90@gmail.com>
2025-03-11 12:31:48 +0100

    oprettet og løst første opgave

---
+++ Models/CounterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorCascadingAndLogging.Models
+{
+    public class CounterModel
+    {
+        public int Count { get; set; } = 0;
+        public event Action? OnCountChanged;
+
+        public void IncrementCount()
+        {
+            Count++;
+            OnCountChanged?.Invoke();
+        }
+    }
+}
