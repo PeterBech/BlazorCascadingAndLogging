3b2f7c5302d97baf3ac2acaf467e77ef0017e259
PeterBech <pibm90@gmail.com>
2025-03-11 14:52:28 +0100

    Logging i konsol

--- obj/Debug/net9.0/BlazorCascadingAndLogging.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorCascadingAndLogging.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCascadingAndLogging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+715c7f002eb70a169f30c343dc133687a632b043")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCascadingAndLogging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCascadingAndLogging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
